cc23b14f91c969db65a6e3818f6ce4f16b6c1f9c
Sacha LAHLOU <slahlou@e3r8p4.clusters.42paris.fr>
2022-11-25 16:59:11 +0100

    char_traits seems ok

--- vector/main.cpp
+++ vector/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <typeinfo>
 #include "vector.hpp"
 #define NL "\n"
 
@@ -12,7 +13,10 @@ int main( void )
 	while (it != test.end())
 		std::cout << "value " << i++ << " : " << *it++ << NL;
 
-	std::cout << "CHAR_TRAITS TEST\n : sizeof(ft::iterator_traits<long *>::value_type) : " << sizeof(ft::iterator_traits<long *>::value_type) << NL;
-	std::cout << << NL;
+	std::cout << "CHAR_TRAITS TEST : \nsizeof(ft::iterator_traits<long *>::value_type) : " << sizeof(ft::iterator_traits<long *>::value_type) << NL;
+	if (typeid(ft::iterator_traits<ft::vector<int>::iterator>::iterator_category) == typeid(std::bidirectional_iterator_tag))
+		std::cout << "[ft::iterator_traits<long *>::iterator_category] : test VRAI" << NL;
+	else
+		std::cout << "[ft::iterator_traits<long *>::iterator_category] : test FAUX" << NL;
 	return (0);
 }
