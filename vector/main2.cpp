3d89f7dbc529cd5cc78d883e11006bc5a71a1c0a
Sacha LAHLOU <slahlou@e3r8p4.clusters.42paris.fr>
2022-12-05 12:04:56 +0100

    stack seems all implemented, test must be pass still

--- vector/main2.cpp
+++ vector/main2.cpp
@@ -1,52 +1,33 @@
 #include <vector>
+#include "stack.hpp"
+// #include <stack>
 #include <iostream>
 
 #define BN "\n"
 
 int main(void){
-	std::vector<int> v;
-	std::vector<int> v2;
+	ft::stack<int> s;
+	ft::stack<int> s2;
 
-	int t[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
-	int f[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 98};
-	v.insert(v.begin(), t, t + 10);
-	v2.insert(v2.begin(), f, f + 10);
+	for(int i = 0; i < 5; i++) {
+		s.push(42 + i);
+		s2.push(42 + i);
+	}
 
-	std::cout << "v" << "  " << "v2" << BN;
-	std::cout << "--" << "  " << "--" << BN;
-	for (int i = 0; i < 10; i++)
-		std::cout << v[i] << "  " << v2[i] << BN;
-	std::cout << BN;
+	std::cout << std::boolalpha;
+	std::cout << (s != s2) << BN;
+	std::cout << (s == s2) << BN;
+	std::cout << (s < s2) << BN;
+	std::cout << (s <= s2) << BN;
+	std::cout << (s > s2) << BN;
+	std::cout << (s >= s2) << BN;
 
-	if (v < v2)
-		std::cout << "v < v2 : true\n";
-	else
-		std::cout << "v < v2 : false\n";
+	std::cout << "printing the content of my stack" << BN;
+	for(; !s.empty(); ) {
+		std::cout << "s.top : " << s.top() << BN;
+		s.pop();
+	}
 
-	if (v > v2)
-		std::cout << "v > v2 : true\n";
-	else
-		std::cout << "v > v2 : false\n";
-
-	if (v == v2)
-		std::cout << "v == v2 : true\n";
-	else
-		std::cout << "v == v2 : false\n";
-
-	if (v != v2)
-		std::cout << "v != v2 : true\n";
-	else
-		std::cout << "v != v2 : false\n";
-
-	if (v <= v2)
-		std::cout << "v <= v2 : true\n";
-	else
-		std::cout << "v <= v2 : false\n";
-
-	if (v >= v2)
-		std::cout << "v >= v2 : true\n";
-	else
-		std::cout << "v >= v2 : false\n";
 
 	return (0);
 }
---
+++ vector/stack.hpp
@@ -0,0 +1,96 @@
+#ifndef STACK_HPP
+#define STACK_HPP
+
+#include "vector.hpp"
+
+namespace ft {
+
+	template <typename T, class Container = ft::vector<T> >
+	class stack {
+		public :
+			typedef	Container								container_type;
+			typedef	typename	Container::value_type		value_type;
+			typedef	typename	Container::size_type		size_type;
+			typedef	typename	Container::reference		reference;
+			typedef	typename	Container::const_reference	const_reference;
+
+			explicit stack (const container_type& ctnr = container_type()) : c(ctnr) {
+				/*  nothing to put for the moment  */
+			}
+
+			bool empty() const {
+				return (c.empty());
+			}
+
+			size_type size() const {
+				return (c.size());
+			}
+
+			reference top() {
+				return ((c[c.size() - 1]));
+			}
+
+			const_reference top() const {
+				return ((c[c.size() - 1]));
+			}
+
+			void push (const value_type& val) {
+				c.push_back(val);
+			}
+
+			void pop() {
+				c.pop_back();
+			}
+
+			/* ***  need friend declaration so my non member overload may access the private container c  *** */
+
+			template <class U, class V>
+			friend bool operator==(const stack<U,V>& lhs, const stack<U,V>& rhs);
+			template <class U, class V>
+			friend bool operator!=(const stack<U,V>& lhs, const stack<U,V>& rhs);
+			template <class U, class V>
+			friend bool operator<(const stack<U,V>& lhs, const stack<U,V>& rhs);
+			template <class U, class V>
+			friend bool operator<=(const stack<U,V>& lhs, const stack<U,V>& rhs);
+			template <class U, class V>
+			friend bool operator>(const stack<U,V>& lhs, const stack<U,V>& rhs);
+			template <class U, class V>
+			friend bool operator>=(const stack<U,V>& lhs, const stack<U,V>& rhs);
+
+		private :
+			Container c;
+	};
+
+	template <class U, class V>
+	bool operator== (const stack<U,V>& lhs, const stack<U,V>& rhs){
+		return (lhs.c == rhs.c);
+	}
+
+	template <class U, class V>
+	bool operator!= (const stack<U,V>& lhs, const stack<U,V>& rhs){
+		return (lhs.c != rhs.c);
+	}
+
+	template <class U, class V>
+	bool operator<  (const stack<U,V>& lhs, const stack<U,V>& rhs){
+		return (lhs.c < rhs.c);
+	}
+
+	template <class U, class V>
+	bool operator<= (const stack<U,V>& lhs, const stack<U,V>& rhs){
+		return (lhs.c <= rhs.c);
+	}
+
+	template <class U, class V>
+	bool operator>  (const stack<U,V>& lhs, const stack<U,V>& rhs){
+		return (lhs.c > rhs.c);
+	}
+
+	template <class U, class V>
+	bool operator>= (const stack<U,V>& lhs, const stack<U,V>& rhs){
+		return (lhs.c >= rhs.c);
+	}
+}
+
+
+#endif
