2bc56e7589dd4498cec8fd7fd47c0b9af9a0e8c4
Sacha LAHLOU <slahlou@e1r7p15.clusters.42paris.fr>
2022-12-08 17:31:34 +0100

    RedBlack Balance function on going

--- ft_containers/main2.cpp
+++ ft_containers/main2.cpp
@@ -20,6 +20,13 @@ int main(void){
 	test.insert(ft::make_pair(13, 0));
 	test.insert(ft::make_pair(17, 0));
 	test.insert(ft::make_pair(12, 0));
+	test.insert(ft::make_pair(11, 0));
+	test.insert(ft::make_pair(55, 0));
+	test.insert(ft::make_pair(25, 0));
+	test.insert(ft::make_pair(34, 0));
+	test.insert(ft::make_pair(18, 0));
+	test.insert(ft::make_pair(23, 0));
+	test.insert(ft::make_pair(42, 0));
 
 
 	test.printBT();
